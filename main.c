6c0164d4d67732e97e0a0a1c8d79788616ee912a
Vinicius Francischini <vfrancischini@Outlook.com>
2025-02-18 20:54:13 -0300

    Entrada e armazenamento das notas

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main(){
+    float nota1, nota2, nota3, media;
+
+    printf("===== Calcule sua média =====\n\n");
+
+    printf("Digite a primeira note: ");
+    scanf("%f", &nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+
+    printf("Digite a terceira nota: ");
+    scanf("%f", &nota3);
+
     return 0;
 }
